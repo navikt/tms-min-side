8bc4c3d6040cb42c361529584207109da190666c
sasoria <sergio.alberto.arevalo.soria@nav.no>
2022-04-07 14:23:01 +0200

    Setter opp en event-emitter for å gjøre det mulig å publisere nivået til mikrofrontends

--- src/microfrontend/renderMinSide.jsx
+++ src/microfrontend/renderMinSide.jsx
@@ -1,15 +1,28 @@
 import React from "react";
 import ErrorBoundary from "../components/errorboundary/ErrorBoundary";
 import ContentLoader from "../components/loader/ContentLoader";
+import { createNanoEvents } from "nanoevents";
+import { useQuery } from "react-query";
+import { minSideProxyUrl } from "../urls";
+import { fetcher } from "../api/api";
 
 export const renderMinSide = (MinSideTopp, MinSideBunn) => {
+  const { data: status, isLoading } = useQuery(`${minSideProxyUrl}/login/status`, fetcher);
+  const emitter = createNanoEvents();
+
+  if (!isLoading) {
+    emitter.on("loaded", () => {
+      emitter.emit("level", status.level);
+    });
+  }
+
   return (
     <React.Suspense fallback={<ContentLoader />}>
       <ErrorBoundary>
-        <MinSideTopp />
+        <MinSideTopp emitter={emitter} />
       </ErrorBoundary>
       <ErrorBoundary>
-        <MinSideBunn />
+        <MinSideBunn emitter={emitter} />
       </ErrorBoundary>
     </React.Suspense>
   );
