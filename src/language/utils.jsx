70156bf73dbd7ac5099aa4cbfb2c953025dbb43b
sasoria <sergio.alberto.arevalo.soria@nav.no>
2022-05-25 13:51:09 +0200

    Legger til en språksjekk for språk som støttes i min-side

--- src/language/utils.jsx
+++ src/language/utils.jsx
@@ -1,7 +1,19 @@
 import Cookies from "js-cookie";
 
+const hasLanguage = (language) => {
+  return typeof language !== "undefined";
+};
+
+const hasAvailableLanguage = (language) => {
+  if (!hasLanguage(language)) {
+    return false;
+  }
+
+  return language.includes("nb") || language.includes("en");
+};
+
 export const getLanguageFromCookie = () => {
   const language = Cookies.get("decorator-language");
 
-  return language ? language : "nb";
+  return hasAvailableLanguage(language) ? language : "nb";
 };
