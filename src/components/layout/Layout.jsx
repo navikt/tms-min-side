fd4a51c8da0ab5f980e295af8f1521b22c87a545
sasoria <sergio.alberto.arevalo.soria@nav.no>
2022-08-15 15:41:11 +0200

    Bruker css-moduler for å scope css slik at man unngår overlapp mellom css-stiler fra flere apper

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import FeilMelding from "../feilmelding/Feilmelding";
-import "./Layout.module.css";
 import { useLanguage } from "../../hooks/useLanguage";
+import styles from "./Layout.module.css";
 
 const Layout = ({ children, isError }) => {
   useLanguage();
 
   return (
-    <div className="layout">
+    <div className={styles["layout"]}>
       <main>
         {isError ? <FeilMelding /> : null}
         {children}
