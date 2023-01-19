836f8728016f5406c7dd4c0007a86899e01c81de
sasoria <sergio.alberto.arevalo.soria@nav.no>
2023-01-19 12:02:05 +0100

    Henter AiA via et manifest for å gjøre det enklere å migrere AiA over til cdn

--- src/mikrofrontend/MinSide.jsx
+++ src/mikrofrontend/MinSide.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import ErrorBoundary from "../components/error-boundary/ErrorBoundary";
 import ContentLoader from "../components/loader/ContentLoader";
 import { minSideTjenesterUrl, minSideOversiktUrl, meldekortMikrofrontendUrl } from "../urls";
-import { arbeidsflateForInnlogetArbeidssokerUrl, arbeidssokerUrl } from "../urls";
+import {
+  arbeidssokerUrl,
+  arbeidsflateForInnloggetArbeidssokerBaseUrl,
+  arbeidsflateForInnloggetArbeidssokerManifestUrl,
+} from "../urls";
 import useStore, { selectIsError, selectSetIsError } from "../store/store";
 import { useBreadcrumbs } from "../hooks/useBreadcrumbs";
 import { useQuery } from "react-query";
@@ -10,20 +14,30 @@ import { fetcher } from "../api/api";
 import { updateUserProperties } from "../amplitude/amplitude";
 import Layout from "../components/layout/Layout";
 
-const MinSideTjenester = React.lazy(() => import(minSideTjenesterUrl));
-const ArbeidsflateForInnlogetArbeidssoker = React.lazy(() => import(arbeidsflateForInnlogetArbeidssokerUrl));
-const MinSideOversikt = React.lazy(() => import(minSideOversiktUrl));
-const Meldekort = React.lazy(() => import(meldekortMikrofrontendUrl));
-
 const MinSide = () => {
   const { data } = useQuery(arbeidssokerUrl, fetcher, {
     onError: useStore(selectSetIsError),
     onSuccess: (data) => updateUserProperties(data.erArbeidssoker),
   });
 
+  const { data: aiaManifest, isLoading: isLoadingTjenesterManifest } = useQuery(
+    arbeidsflateForInnloggetArbeidssokerManifestUrl,
+    fetcher
+  );
+
   const isError = useStore(selectIsError);
   useBreadcrumbs();
 
+  if (isLoadingTjenesterManifest) {
+    return <ContentLoader />;
+  }
+
+  const ArbeidsflateForInnlogetArbeidssoker = React.lazy(() =>
+    import(`${arbeidsflateForInnloggetArbeidssokerBaseUrl}/${aiaManifest["src/main.tsx"]["file"]}`)
+  );
+  const MinSideOversikt = React.lazy(() => import(minSideOversiktUrl));
+  const Meldekort = React.lazy(() => import(meldekortMikrofrontendUrl));
+  const MinSideTjenester = React.lazy(() => import(minSideTjenesterUrl));
   return (
     <Layout isError={isError}>
       <React.Suspense fallback={<ContentLoader />}>
