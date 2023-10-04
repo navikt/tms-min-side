1348ee3aceb6d2fe93f96602cee152afbef5c490
sasoria <sergio.alberto.arevalo.soria@nav.no>
2023-10-04 11:28:34 +0200

    Bruk arbeidssoker endepunktet

--- src/mikrofrontends/arbeidssoker/Arbeidssokerflate.tsx
+++ src/mikrofrontends/arbeidssoker/Arbeidssokerflate.tsx
@@ -1,16 +1,28 @@
 import React from "react";
 import ContentLoader from "../../components/loader/ContentLoader";
-import { aiaCdnUrl, aiaManifestUrl } from "./urls";
+import { aiaCdnUrl, aiaManifestUrl, arbeidssokerUrl } from "./urls";
 import { aiaEntry, bundle } from "../entrypoints";
 import { useManifest } from "../../hooks/useManifest";
 import ErrorBoundary from "../../components/error-boundary/ErrorBoundary";
+import useSWRImmutable from "swr/immutable";
+import { fetcher } from "../../utils/api.client.ts";
 
 const Arbeidssokerflate = () => {
-  const [manifest, isLoading] = useManifest(aiaManifestUrl);
+  const [manifest, isLoadingManifest] = useManifest(aiaManifestUrl);
+  const { data: arbeidssoker, isLoading: isLoadingArbeidssoker } = useSWRImmutable({ path: arbeidssokerUrl }, fetcher);
 
-  if (isLoading) {
+  if (isLoadingArbeidssoker) {
+    return null;
+  }
+
+  if (!arbeidssoker?.erArbeidssoker) {
+    return null;
+  }
+
+  if (isLoadingManifest) {
     return <ContentLoader />;
   }
+
 
   const ArbeidsflateForInnloggetArbeidssoker = React.lazy(() => import(`${aiaCdnUrl}/${manifest[aiaEntry][bundle]}`));
 
