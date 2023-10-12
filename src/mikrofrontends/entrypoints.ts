ff10e086415197af78f3cefff204358d9d880329
sasoria <sergio.alberto.arevalo.soria@nav.no>
2023-10-12 19:40:06 +0200

    Clean up

--- src/mikrofrontends/entrypoints.ts
+++ src/mikrofrontends/entrypoints.ts
@@ -1,5 +1,4 @@
 export const aiaEntry = "src/main.tsx";
-export const aiaStandardEntry = "src/main-standard.tsx";
 export const oversiktEntry = "src/Mikrofrontend.jsx";
 export const tidligereVarslerEntry = "src/Mikrofrontend.jsx";
 export const entry = "src/Mikrofrontend.tsx";
