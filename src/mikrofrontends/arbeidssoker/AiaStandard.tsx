c51a656da1aab67ffc0e3f83ebe18552b2fbee4c
sasoria <sergio.alberto.arevalo.soria@nav.no>
2023-10-12 13:09:41 +0200

    Test aia for standard

---
+++ src/mikrofrontends/arbeidssoker/AiaStandard.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import ContentLoader from "../../components/loader/ContentLoader";
+import { aiaCdnUrl, aiaManifestUrl, arbeidssokerUrl, standardUrl } from "./urls";
+import { aiaEntry, bundle } from "../entrypoints";
+import { useManifest } from "../../hooks/useManifest";
+import ErrorBoundary from "../../components/error-boundary/ErrorBoundary";
+import useSWRImmutable from "swr/immutable";
+import { fetcher } from "../../utils/api.client.ts";
+
+const AiaStandard = () => {
+  const { data: arbeidssoker, isLoading: isLoadingArbeidssoker } = useSWRImmutable({ path: standardUrl }, fetcher);
+  const [manifest, isLoadingManifest] = useManifest(aiaManifestUrl);
+
+  if (isLoadingArbeidssoker) {
+    return null;
+  }
+
+  if (!arbeidssoker?.erArbeidssoker) {
+    return null;
+  }
+
+  if (isLoadingManifest) {
+    return null;
+  }
+
+
+  const AiaStandard = React.lazy(() => import(`${aiaCdnUrl}/${manifest[aiaEntry][bundle]}`));
+
+  return (
+    <React.Suspense fallback={<ContentLoader />}>
+      <ErrorBoundary>
+        <AiaStandard />
+      </ErrorBoundary>
+    </React.Suspense>
+  );
+};
+
+export default AiaStandard;
