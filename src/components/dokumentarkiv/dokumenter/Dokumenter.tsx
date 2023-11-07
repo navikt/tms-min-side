f534b0bff28e9409845456403736df42dab53fce
sasoria <sergio.alberto.arevalo.soria@nav.no>
2023-11-07 10:15:41 +0100

    Nytt design på ingen dokumenter

--- src/components/dokumentarkiv/dokumenter/Dokumenter.tsx
+++ src/components/dokumentarkiv/dokumenter/Dokumenter.tsx
@@ -10,6 +10,7 @@ import type { Language } from "@language/language.ts";
 import { text } from "@language/dokumentarkiv.ts";
 import { logEvent } from "@utils/amplitude.ts";
 import styles from "./Dokumenter.module.css";
+import { Heading } from "@navikt/ds-react";
 
 interface Dokument {
   navn: string;
@@ -78,18 +79,23 @@ const Dokumenter = ({ language }: Props) => {
           </BodyShort>
         </div>
       ) : (
-        <>
+        <div className={styles.dokumentContainer}>
           <BodyShort as="h2" spacing={true}>
             {text.heading[language]}
           </BodyShort>
-          <div className={styles.ingenContainer}>
-            <FileTextIcon aria-hidden fontSize="32px" />
-            <BodyLong>{text.ingen[language]}</BodyLong>
+          <div className={styles.skeletonContainer}>
+            <Heading size="small" as="h3">
+              {text.ingenHeading[language]}
+            </Heading>
+            <BodyLong className={styles.ingenText}>
+              {text.ingenText[language]}
+            </BodyLong>
           </div>
-          <a className={styles.link} href={dokumentarkivUrl} >
-            {text.dokumentarkiv[language]}
-          </a>
-        </>
+          <div className={styles.skeletonContainer} />
+            <a className={styles.link} href={dokumentarkivUrl} >
+              {text.dokumentarkiv[language]}
+            </a>
+          </div>
       )}
     </>
   );
