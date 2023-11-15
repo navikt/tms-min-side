ca0505639e9c0b194e4c54e4f00711da6735c331
sasoria <sergio.alberto.arevalo.soria@nav.no>
2023-11-15 14:47:28 +0100

    Fjern query params

--- src/components/utbetaling/utbetalingUrls.ts
+++ src/components/utbetaling/utbetalingUrls.ts
@@ -1,8 +1,4 @@
-import dayjs from "dayjs";
 import { getEnvironmentClientSide } from "@utils/environment.client.ts";
-
-const toDate = dayjs().format("YYYYMMDD");
-const fromDate = dayjs().subtract(3, "month").format("YYYYMMDD");
 
 const UTBETALINGSOVERSIKT_URL = {
   local: "http://localhost:3000/utbetalingsoversikt",
@@ -16,5 +12,5 @@ const UTBETALINGSOVERSIKT_API_URL = {
   prod: "https://person.nav.no/tms-utbetalingsoversikt-api/utbetalinger/siste",
 };
 
-export const utbetalingsoversiktApiUrl = `${UTBETALINGSOVERSIKT_API_URL[getEnvironmentClientSide()]}?&fom=${fromDate}&tom=${toDate}`;
+export const utbetalingsoversiktApiUrl = UTBETALINGSOVERSIKT_API_URL[getEnvironmentClientSide()];
 export const utbetalingsoversiktUrl = UTBETALINGSOVERSIKT_URL[getEnvironmentClientSide()];
