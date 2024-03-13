6fedaa7aa3e08d211846f7b4f72ff32ce323ba94
rannveigskjerve <rannveig.alette.skjerve@nav.no>
2024-03-13 11:05:15 +0100

    bruk response fra selector for å velge produktkort
    Co-authored-by: sasoria <sergio.alberto.arevalo.soria@nav.no>

--- src/components/oversikt/din-oversikt/microfrontendTypes.tsx
+++ src/components/oversikt/din-oversikt/microfrontendTypes.tsx
@@ -1,9 +1,14 @@
+export interface Sakstemaer {
+  kode: string;
+}
+
 export interface EnabledMicrofrontend {
   microfrontend_id: string;
   url: string;
 }
 
-export interface EnabledMicrofrontends {
+export interface PersonalizedContent {
   microfrontends: EnabledMicrofrontend[];
+  produktkort: Sakstemaer[];
   offerStepup: boolean;
 }
