35e10974604bb5c0c038ab3016ed675b3a508a1a
Amirhossien Lotfi <amirhossien.lotfi@nav.no>
2024-05-08 07:51:54 +0200

    Reload ved page transition for å unngå at browser viser cachet innhold

--- src/components/authentication/Authentication.tsx
+++ src/components/authentication/Authentication.tsx
@@ -2,8 +2,18 @@ import useSWRImmutable from "swr/immutable"
 import { redirectUrl, statusUrl } from "./urls";
 import { fetcher } from "@utils/api.client";
 
+export const reloadOnPageshow = () => {
+  window.addEventListener("pageshow", (event) => {
+    if (event.persisted) {
+      location.reload();
+    }
+  });
+};
+
 const Authentication = () => {
   const { data } = useSWRImmutable({ path : statusUrl }, fetcher);
+
+  reloadOnPageshow()
   if (data?.authenticated === false) {
     window.location.assign(redirectUrl)
   }
