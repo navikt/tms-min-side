d6ac384e252efca34de74563dc191b5859e0eb4e
Amirhossien Lotfi <amirhossien.lotfi@nav.no>
2024-06-07 12:27:07 +0200

    Formating

--- src/utils/environment.client.ts
+++ src/utils/environment.client.ts
@@ -18,8 +18,8 @@ export const getEnvironmentClientSide = () => {
   return "prod";
 };
 
-const devInternUrl: string = "https://www.intern.dev.nav.no"
-const devAnsattUrl: string = "https://www.ansatt.dev.nav.no"
+const devInternUrl: string = "https://www.intern.dev.nav.no";
+const devAnsattUrl: string = "https://www.ansatt.dev.nav.no";
 export const getDevBaseUrl =
-  window.location.href.includes("intern.dev.nav.no") ? devInternUrl: devAnsattUrl;
+  window.location.href.includes("intern.dev.nav.no") ? devInternUrl : devAnsattUrl;
 
