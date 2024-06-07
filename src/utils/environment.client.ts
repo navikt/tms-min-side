5026a116c164de79412c201e4e1b1446d54fe0c1
Amirhossien Lotfi <amirhossien.lotfi@nav.no>
2024-06-07 10:26:23 +0200

    Gjøre dev tilgjengelig bak ansatt url

--- src/utils/environment.client.ts
+++ src/utils/environment.client.ts
@@ -2,7 +2,7 @@ let isDevelopmentClientSide:boolean = false;
 let isLocalClientSide:boolean = false;
 
 if (!import.meta.env.SSR) {
-  isDevelopmentClientSide = window.location.href.includes("www.intern.dev.nav.no");
+  isDevelopmentClientSide = window.location.href.includes("dev.nav.no") ;
   isLocalClientSide = process.env.NODE_ENV === "development";
 }
 
