17928a22df93cd5933b4000bedcb029d7d5bb42a
Sergio A. Arevalo Soria <sergio.alberto.arevalo.soria@nav.no>
2024-08-05 10:25:11 +0200

    Fjern msw

--- vitest.config.ts
+++
@@ -1,9 +0,0 @@
-import { getViteConfig } from 'astro/config';
-
-export default getViteConfig({
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: ["vitest-setup.tsx"],
-  }
-});
