164be67c237726810a2fd702f87314c320670cb6
sasoria <sergio.alberto.arevalo.soria@nav.no>
2024-08-15 19:09:50 +0200

    Fix url

--- src/components/personalia/personaliaUrls.ts
+++ src/components/personalia/personaliaUrls.ts
@@ -2,8 +2,8 @@ import { getEnvironment } from "@utils/server/environment.ts";
 
 const NAVN_URL = {
   local: "http://localhost:3000/navn",
-  dev: "http://tms-min-side-proxy.min-side/tms-min-side-proxy/navn",
-  prod: "http://tms-min-side-proxy.min-side/tms-min-side-proxy/navn",
+  dev: "http://tms-min-side-proxy/tms-min-side-proxy/navn",
+  prod: "http://tms-min-side-proxy/tms-min-side-proxy/navn",
 };
 
 export const navnUrl = NAVN_URL[getEnvironment()];
