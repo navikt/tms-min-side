5980b245c2d1f01776a44747f85e9879c8c86797
sasoria <sergio.alberto.arevalo.soria@nav.no>
2024-08-16 11:40:32 +0200

    Debug logging

--- src/utils/server/token.ts
+++ src/utils/server/token.ts
@@ -5,7 +5,7 @@ const audience = `${process.env.NAIS_CLUSTER_NAME}:min-side:tms-min-side-proxy`;
 
 export const getOboToken = async (token: string): Promise<string> => {
   const oboResult = await requestOboToken(token, audience);
-
+  console.info("Audience: " + audience);
   if (isLocal) {
     return "Fake token";
   }
