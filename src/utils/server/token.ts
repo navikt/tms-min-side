2e4405b29c0d957f89692a13e5b4f3671db8cdd2
sasoria <sergio.alberto.arevalo.soria@nav.no>
2024-08-26 09:59:05 +0200

    Fjern debug logging

--- src/utils/server/token.ts
+++ src/utils/server/token.ts
@@ -6,17 +6,13 @@ const audience = `${process.env.NAIS_CLUSTER_NAME}:min-side:tms-min-side-proxy`;
 export const getOboToken = async (token: string): Promise<string> => {
   const oboResult = await requestOboToken(token, audience);
 
-  console.info("Audience: " + audience);
-  console.info("Idporten: " + process.env.IDPORTEN_ISSUER)
-
   if (isLocal) {
     return "Fake token";
   }
 
   if (!oboResult.ok) {
-    console.log("Fail on-behalf-of token for api");
-    console.log("Error: " + oboResult.error);
-    throw new Error("Request oboToken for min-side-proxy backend failed");
+    console.error("Error getting access token: " + oboResult.error);
+    throw new Error("Request oboToken for min-side-proxy failed ");
   }
 
   return oboResult.token;
