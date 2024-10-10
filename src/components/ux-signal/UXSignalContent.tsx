8262bf6b7a040cb84623c3d179495313db1b6bb3
Jostein Holje <jostein.holje@nav.no>
2024-10-10 11:13:21 +0200

    UXSignals widget for AiA

---
+++ src/components/ux-signal/UXSignalContent.tsx
@@ -0,0 +1,14 @@
+import useUXSignalScript from "./useUXSignalScript.tsx";
+
+interface Props {
+  panelId: string;
+  mode?: 'demo' | '';
+}
+
+const UXSignalContent = (props: Props) => {
+  useUXSignalScript(true);
+
+  return <div data-uxsignals-embed={props.panelId} style={{ maxWidth: "620px" }} data-uxsignals-mode={props.mode ?? ''}/>;
+};
+
+export default UXSignalContent;
