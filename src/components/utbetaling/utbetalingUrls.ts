d1a383b4f60fa5b97e1ff6278d9bb8046377b0bf
Nima Hakimi <nima.hakimi2@nav.no>
2024-10-29 15:00:52 +0100

    Enre url til den nye astro appen

--- src/components/utbetaling/utbetalingUrls.ts
+++ src/components/utbetaling/utbetalingUrls.ts
@@ -2,13 +2,13 @@ import { getDevBaseUrl, getEnvironmentClientSide } from "@utils/client/environme
 
 const UTBETALINGSOVERSIKT_URL = {
   local: "http://localhost:3000/utbetalingsoversikt",
-  dev: "https://www.intern.dev.nav.no/utbetalingsoversikt",
-  prod: "https://www.nav.no/utbetalingsoversikt",
+  dev: "https://www.intern.dev.nav.no/utbetalingsoversikt-ny",
+  prod: "https://www.nav.no/utbetalingsoversikt-ny",
 };
 
 const UTBETALINGSOVERSIKT_API_URL = {
   local: "http://localhost:3000/utbetalinger/siste",
-  dev:  `${getDevBaseUrl}/tms-utbetalingsoversikt-api/utbetalinger/siste`,
+  dev: `${getDevBaseUrl}/tms-utbetalingsoversikt-api/utbetalinger/siste`,
   prod: "https://person.nav.no/tms-utbetalingsoversikt-api/utbetalinger/siste",
 };
 
