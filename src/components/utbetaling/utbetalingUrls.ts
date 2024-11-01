91f3cd1b017cbc395ce205d93a2ae3d45a844ee7
Nima Hakimi <nima.hakimi2@nav.no>
2024-11-01 10:14:35 +0100

    Revert to original utbetalingsoversikt url

--- src/components/utbetaling/utbetalingUrls.ts
+++ src/components/utbetaling/utbetalingUrls.ts
@@ -2,8 +2,8 @@ import { getDevBaseUrl, getEnvironmentClientSide } from "@utils/client/environme
 
 const UTBETALINGSOVERSIKT_URL = {
   local: "http://localhost:3000/utbetalingsoversikt",
-  dev: "https://www.intern.dev.nav.no/utbetalingsoversikt-ny",
-  prod: "https://www.nav.no/utbetalingsoversikt-ny",
+  dev: "https://www.intern.dev.nav.no/utbetalingsoversikt",
+  prod: "https://www.nav.no/utbetalingsoversikt",
 };
 
 const UTBETALINGSOVERSIKT_API_URL = {
