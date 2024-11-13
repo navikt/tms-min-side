d21f40ae51d9c8abf30279ef1e13b579e4446f0d
sasoria <sergio.alberto.arevalo.soria@nav.no>
2024-11-13 14:25:41 +0100

    Fiks ssr check

--- src/components/oversikt/urls.ts
+++ src/components/oversikt/urls.ts
@@ -1,4 +1,8 @@
-import { getDevBaseUrl, getEnvironmentClientSide, isAnsattClientSide } from "@utils/client/environment.ts";
+import {
+  getIsAnsattClientSide,
+  getDevBaseUrl,
+  getEnvironmentClientSide,
+} from "@utils/client/environment.ts";
 
 const MIN_SIDE_PROXY_URL = {
   local: "http://localhost:3000/tms-min-side-proxy",
@@ -31,7 +35,7 @@ const AKTIVITETSPLAN_URL = {
 };
 
 const meldekortAnsattUrl = "https://arbeid.ansatt.dev.nav.no/meldekort-mikrofrontend/meldekort-mikrofrontend.js";
-export const meldekortUrl = isAnsattClientSide ? meldekortAnsattUrl : MELDEKORT_URL[getEnvironmentClientSide()];
+export const meldekortUrl = getIsAnsattClientSide() ? meldekortAnsattUrl : MELDEKORT_URL[getEnvironmentClientSide()];
 export const dinOversiktUrl = `${SELECTOR_URL[getEnvironmentClientSide()]}/din-oversikt`;
 export const featureToggleUrl = `${MIN_SIDE_PROXY_URL[getEnvironmentClientSide()]}/featuretoggles`;
 export const dialogMedVeilederUrl = DIALOG_MED_VEILEDER_URL[getEnvironmentClientSide()];
--- src/utils/client/environment.ts
+++ src/utils/client/environment.ts
@@ -1,16 +1,20 @@
 let isDevelopmentClientSide: boolean = false;
 let isLocalClientSide: boolean = false;
+let isAnsattClientSide = false;
 let devBaseUrl: string = "https://www.ansatt.dev.nav.no";
-
-export const isAnsattClientSide = window.location.href.includes("ansatt.dev.nav.no");
 
 if (!import.meta.env.SSR) {
   isDevelopmentClientSide = window.location.href.includes("dev.nav.no");
   isLocalClientSide = process.env.NODE_ENV === "development";
+  isAnsattClientSide = window.location.href.includes("ansatt.dev.nav.no");
   if(window.location.href.includes("intern.dev.nav.no")){
     devBaseUrl = "https://www.intern.dev.nav.no";
   }
 }
+
+export const getIsAnsattClientSide = () => {
+  return isAnsattClientSide;
+};
 
 export const getEnvironmentClientSide = () => {
   if (isLocalClientSide) {
