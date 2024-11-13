82c47a2703663a854a532334ff72346ce7405060
sasoria <sergio.alberto.arevalo.soria@nav.no>
2024-11-13 14:11:38 +0100

    Ansatt url for meldekort

--- src/components/oversikt/urls.ts
+++ src/components/oversikt/urls.ts
@@ -1,4 +1,4 @@
-import { getDevBaseUrl, getEnvironmentClientSide } from "@utils/client/environment.ts";
+import { getDevBaseUrl, getEnvironmentClientSide, isAnsattClientSide } from "@utils/client/environment.ts";
 
 const MIN_SIDE_PROXY_URL = {
   local: "http://localhost:3000/tms-min-side-proxy",
@@ -30,7 +30,8 @@ const AKTIVITETSPLAN_URL = {
   prod: "https://aktivitetsplan.nav.no/",
 };
 
-export const meldekortUrl = MELDEKORT_URL[getEnvironmentClientSide()];
+const meldekortAnsattUrl = "https://arbeid.ansatt.dev.nav.no/meldekort-mikrofrontend/meldekort-mikrofrontend.js";
+export const meldekortUrl = isAnsattClientSide ? meldekortAnsattUrl : MELDEKORT_URL[getEnvironmentClientSide()];
 export const dinOversiktUrl = `${SELECTOR_URL[getEnvironmentClientSide()]}/din-oversikt`;
 export const featureToggleUrl = `${MIN_SIDE_PROXY_URL[getEnvironmentClientSide()]}/featuretoggles`;
 export const dialogMedVeilederUrl = DIALOG_MED_VEILEDER_URL[getEnvironmentClientSide()];
--- src/utils/client/environment.ts
+++ src/utils/client/environment.ts
@@ -2,6 +2,8 @@ let isDevelopmentClientSide: boolean = false;
 let isLocalClientSide: boolean = false;
 let devBaseUrl: string = "https://www.ansatt.dev.nav.no";
 
+export const isAnsattClientSide = window.location.href.includes("ansatt.dev.nav.no");
+
 if (!import.meta.env.SSR) {
   isDevelopmentClientSide = window.location.href.includes("dev.nav.no");
   isLocalClientSide = process.env.NODE_ENV === "development";
