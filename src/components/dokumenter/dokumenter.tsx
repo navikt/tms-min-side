1d2e9cd9fc73dfc48e2140221d9eaf3f23ae3399
sasoria <sergio.alberto.arevalo.soria@nav.no>
2024-11-18 13:14:04 +0100

    Heading

--- src/components/dokumenter/dokumenter.tsx
+++ src/components/dokumenter/dokumenter.tsx
@@ -4,7 +4,7 @@ import { setIsError } from "../../store/store.ts";
 import { Language } from "@language/language.ts";
 import { Journalpost } from "@components/dokumenter/dokumenterTypes.ts";
 import Dokument from "@components/dokumenter/dokument/dokument.tsx";
-import { BodyShort } from "@navikt/ds-react";
+import { BodyShort, Heading } from "@navikt/ds-react";
 import { ChevronRightIcon } from "@navikt/aksel-icons";
 import styles from "./dokumenter.module.css";
 import { dokumentarkivUrl, journalposterUrl } from "@components/dokumenter/dokumenterUrls.ts";
@@ -25,24 +25,27 @@ const Dokumenter = ({ language }: Props) => {
   }
 
   return (
-    <ul className={styles.dokumenter}>
-      {journalposter?.slice(0, 3).map((jp) => (
-        <Dokument
-          tittel={jp.dokument.tittel}
-          opprettet={jp.opprettet}
-          journalpost={jp}
-          key={jp.dokument.dokumentInfoId}
-        />
-      ))}
-      <div className={styles.alle}>
-        <a className={styles.link} href={dokumentarkivUrl}>
-        <BodyShort size="medium">
-          Se alle
-        </BodyShort>
-        </a>
-        <ChevronRightIcon fontSize="20px" />
-      </div>
-    </ul>
+    <>
+      <Heading size="xsmall" spacing>Dokumentarkiv</Heading>
+      <ul className={styles.dokumenter}>
+        {journalposter?.slice(0, 3).map((jp) => (
+          <Dokument
+            tittel={jp.dokument.tittel}
+            opprettet={jp.opprettet}
+            journalpost={jp}
+            key={jp.dokument.dokumentInfoId}
+          />
+        ))}
+        <div className={styles.alle}>
+          <a className={styles.link} href={dokumentarkivUrl}>
+          <BodyShort size="medium">
+            Se alle
+          </BodyShort>
+          </a>
+          <ChevronRightIcon fontSize="20px" />
+        </div>
+      </ul>
+    </>
   );
 };
 
