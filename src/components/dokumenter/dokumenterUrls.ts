93d9b00b15a0649bcba105011a388ed3f57d1a5e
sasoria <sergio.alberto.arevalo.soria@nav.no>
2024-12-02 14:03:34 +0100

    Fix url

--- src/components/dokumenter/dokumenterUrls.ts
+++ src/components/dokumenter/dokumenterUrls.ts
@@ -9,7 +9,7 @@ const DOKUMENTARKIV_URL = {
 const DOKUMENTER_URL = {
   local: "http://localhost:3000/journalposter",
   dev: `${getDevBaseUrl}/mine-saker-api/v2/journalposter/siste`,
-  prod: "https://www.nav.no/mine-saker-api/v2/journalposter/siste",
+  prod: "https://person.nav.no/mine-saker-api/v2/journalposter/siste",
 };
 
 export const dokumentarkivUrl = DOKUMENTARKIV_URL[getEnvironmentClientSide()];
