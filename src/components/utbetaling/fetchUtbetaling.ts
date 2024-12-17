d367272f1604cf98b26635cc353a3588a086c573
sasoria <sergio.alberto.arevalo.soria@nav.no>
2024-12-17 18:55:53 +0100

    Forenkler fetch utbetaling

--- src/components/utbetaling/fetchUtbetaling.ts
+++ src/components/utbetaling/fetchUtbetaling.ts
@@ -1,35 +1,19 @@
-import type { UtbetalingResponse } from "./utbetalingTypes";
+import type { Utbetaling } from "./utbetalingTypes";
 import { utbetalingsoversiktApiUrl } from "./utbetalingUrls";
 
-export const fetchUtbetaling = async (oboToken: string) => {
-  let isError =  false;
-
-  const response: UtbetalingResponse = await fetch(utbetalingsoversiktApiUrl, {
+export const fetchUtbetaling = async (oboToken: string): Promise<Utbetaling> => {
+  const response = await fetch(utbetalingsoversiktApiUrl, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
       Accept: "application/json",
       Authorization: `Bearer ${oboToken}`,
     },
-  }).then((response) => {
-    if (!response.ok) {
-      console.error(`Error, fetch failed with status: ${response.status}`);
-      isError = true;
-      return;
-    }
-
-    return response.json();
-  }).catch((error) => {
-    console.error(`Error, fetch failed: ${error}`);
-    isError = true;
   });
 
-  const data = isError ?
-    {
-      hasKommende: false,
-      hasUtbetaling: false,
-      sisteUtbetaling: null,
-      kommende: null
-    } : response;
+  if (!response.ok) {
+    throw new Error(`Http error with status: ${response.status}`);
+  }
 
+  return await response.json();
 }
