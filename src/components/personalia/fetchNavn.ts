f35de60b7f2ec1162cd131df15e423076f258be4
sasoria <sergio.alberto.arevalo.soria@nav.no>
2024-12-17 17:19:14 +0100

    Refactor fetching av navn

--- src/components/personalia/fetchNavn.ts
+++ src/components/personalia/fetchNavn.ts
@@ -1,8 +1,6 @@
 import { navnUrl } from "./personaliaUrls";
 
 export const fetchNavn = async (oboToken: string) => {
-  let isError =  false;
-
   const response = await fetch(navnUrl, {
     method: "GET",
     headers: {
@@ -10,20 +8,11 @@ export const fetchNavn = async (oboToken: string) => {
       Accept: "application/json",
       Authorization: `Bearer ${oboToken}`,
     },
-  }).then((response) => {
-    if (!response.ok) {
-      console.error(`Error, fetch failed with status: ${response.status}`);
-      isError = true;
-      return;
-    }
-
-    return response.json();
-  }).catch((error) => {
-    console.error(`Error, fetch failed: ${error}`);
-    isError = true;
   });
 
-  const data = isError ? { navn: "" } : response;
+  if (!response.ok) {
+    throw new Error(`Http error with status: ${response.status}`);
+  }
 
-  return { data, isError };
+  return await response.json();
 }
