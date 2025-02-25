15df664736005473c2b10bdaba57365f9072376f
sasoria <sergio.alberto.arevalo.soria@nav.no>
2025-02-25 19:17:02 +0100

    Bruk pino for logging

--- src/utils/server/token.ts
+++ src/utils/server/token.ts
@@ -1,15 +1,17 @@
 import { requestOboToken } from "@navikt/oasis";
 import { isLocal } from "@utils/server/environment.ts";
+import pino from "pino-http";
 
 export const getOboToken = async (token: string, audience: string): Promise<string> => {
   const oboResult = await requestOboToken(token, audience);
+  const logger = pino().logger;
 
   if (isLocal) {
     return "Fake token";
   }
 
   if (!oboResult.ok) {
-    console.error("Error getting access token: " + oboResult.error);
+    logger.error("Error getting access token: " + oboResult.error);
     throw new Error("Request oboToken for min-side-proxy failed ");
   }
 
