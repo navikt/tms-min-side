f6d896a9a644916f9acb34828f1622314682206e
sasoria <sergio.alberto.arevalo.soria@nav.no>
2025-02-27 12:28:43 +0100

    Oppdater referanser til nav

--- src/language/innboks.ts
+++ src/language/innboks.ts
@@ -15,9 +15,9 @@ export const text = {
     en: "Inbox",
   },
   kommunikasjonsFlisIngressInnboks: {
-    nb: "Informasjon fra NAV og svar på henvendelser og referater fra samtaler du har på telefon, chat og “Skriv til oss”.",
-    nn: "Informasjon frå NAV og svar på førespurnadar og referat frå samtalar du har på telefon, chat og “Skriv til oss”.",
-    en: "Information from NAV and replies to inquiries, and summaries of conversations via phone calls, chats and “Write to us”.",
+    nb: "Informasjon fra Nav og svar på henvendelser og referater fra samtaler du har på telefon, chat og “Skriv til oss”.",
+    nn: "Informasjon frå Nav og svar på førespurnadar og referat frå samtalar du har på telefon, chat og “Skriv til oss”.",
+    en: "Information from Nav and replies to inquiries, and summaries of conversations via phone calls, chats and “Write to us”.",
   },
   innboksIngenNyMeldinger: {
     nb: "Ingen nye meldinger",
