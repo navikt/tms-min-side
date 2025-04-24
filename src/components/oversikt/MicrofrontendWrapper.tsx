099dd4cda2bed14715435875a1cf78b65639e590
Eivinn Eilertsen <eivinne@Eivinns-MacBook-Pro.local>
2025-04-24 10:07:32 +0200

    implementert masonry via css only

--- src/components/oversikt/MicrofrontendWrapper.tsx
+++ src/components/oversikt/MicrofrontendWrapper.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ErrorBoundary from "@components/error-boundary/ErrorBoundary.tsx";
 import { Skeleton } from "@navikt/ds-react";
+import styles from "./MicrofrontendWrapper.module.css";
 
 const MicrofrontendWrapper = ({ manifestUrl }: { manifestUrl: string }) => {
   const Microfrontend = React.lazy(() => import(manifestUrl));
@@ -8,7 +9,9 @@ const MicrofrontendWrapper = ({ manifestUrl }: { manifestUrl: string }) => {
   return (
     <React.Suspense fallback={<Skeleton variant="rectangle" width={444} height={124} />}>
       <ErrorBoundary url={manifestUrl}>
-        <Microfrontend />
+        <div className={styles.wrapper}>
+          <Microfrontend />
+        </div>
       </ErrorBoundary>
     </React.Suspense>
   );
