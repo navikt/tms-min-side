6951aa908f9a806f68d4ff8b3a3480f0f2b317fe
sasoria <sergio.alberto.arevalo.soria@nav.no>
2025-05-05 15:05:17 +0200

    Debug

--- src/components/din-oversikt/utils/urls.ts
+++ src/components/din-oversikt/utils/urls.ts
@@ -2,7 +2,7 @@ import { getEnvironment } from "../../../utils/server/environment";
 
 const SELECTOR_URL = {
   local: "http://localhost:3000/selector/din-oversikt",
-  dev: `http://tms-mikrofrontend-selector/selector/din-oversikt`,
+  dev: "http://tms-mikrofrontend-selector/selector/din-oversikt",
   prod: "http://tms-mikrofrontend-selector/selector/din-oversikt",
 };
 
