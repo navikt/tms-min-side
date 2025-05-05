5493326ba7dfcd9b51b4be1a0ee4f1c81a0b04c7
sasoria <sergio.alberto.arevalo.soria@nav.no>
2025-05-05 15:18:16 +0200

    Oppdater selector url

--- src/components/din-oversikt/utils/urls.ts
+++ src/components/din-oversikt/utils/urls.ts
@@ -2,8 +2,8 @@ import { getEnvironment } from "../../../utils/server/environment";
 
 const SELECTOR_URL = {
   local: "http://localhost:3000/selector/din-oversikt",
-  dev: "http://tms-mikrofrontend-selector/selector/din-oversikt",
-  prod: "http://tms-mikrofrontend-selector/selector/din-oversikt",
+  dev: "http://tms-mikrofrontend-selector/din-oversikt",
+  prod: "http://tms-mikrofrontend-selector/din-oversikt",
 };
 
 export const dinOversiktUrl = SELECTOR_URL[getEnvironment()];
