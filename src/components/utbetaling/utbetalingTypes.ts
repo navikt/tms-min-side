a242ba432a67a69c463d158945b2cdd1c3ff43b5
Nima <nima.hakimi@nav.no>
2025-06-20 10:33:50 +0200

    New payment widget with improved architecture

--- src/components/utbetaling/utbetalingTypes.ts
+++ src/components/utbetaling/utbetalingTypes.ts
@@ -1,22 +1,14 @@
-export interface SisteUtbetaling {
+export type UtbetalingData = {
   utbetaling: number;
   dato: string;
   ytelse: string;
   kontonummer: string;
   id: string;
-}
+};
 
-export interface Kommende {
-  utbetaling: number;
-  dato: string;
-  ytelse: string;
-  kontonummer: string;
-  id: string;
-}
-
-export interface Utbetaling {
+export type Utbetaling = {
   hasUtbetaling: boolean;
   hasKommende: boolean;
-  sisteUtbetaling: SisteUtbetaling | null;
-  kommende: Kommende | null;
-}
+  sisteUtbetaling: UtbetalingData | null;
+  kommende: UtbetalingData | null;
+};
