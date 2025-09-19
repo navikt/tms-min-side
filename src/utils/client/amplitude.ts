c3fb4e9c2436d5329a3a4c319f79006d7eae160c
Eivinn Eilertsen <eivinne@Eivinns-MacBook-Pro.local>
2025-09-19 09:48:33 +0200

    Oppdatert syntax i logger til å benytte typescript

--- src/utils/client/amplitude.ts
+++ src/utils/client/amplitude.ts
@@ -1,21 +1,24 @@
-import { getAmplitudeInstance } from "@navikt/nav-dekoratoren-moduler";
+import type { AmplitudeEvent } from "@navikt/nav-dekoratoren-moduler";
+import { getAnalyticsInstance } from "@navikt/nav-dekoratoren-moduler";
 
-const logger = getAmplitudeInstance("dekoratoren");
+type ExtendedAmplitudeEvent = AmplitudeEvent<"navigere", { kategori: string }>;
 
-export const logEvent = (data: string, kategori: string, lenketekst: string) => {
-  logger("navigere", { komponent: data, kategori: kategori, lenketekst: lenketekst }).catch(() =>
+const logger = getAnalyticsInstance<ExtendedAmplitudeEvent>("tms-min-side");
+
+export const logEvent = async (data: string, kategori: string, lenketekst: string) => {
+  await logger("navigere", { komponent: data, kategori: kategori, lenketekst: lenketekst }).catch(() =>
     console.warn("Uninitialized amplitude"),
   );
 };
 
-export function logMfEvent(name: string, metric: boolean) {
-  logger(name, { komponent: metric }).catch(() => console.warn("Uninitialized amplitude"));
+export async function logMfEvent(name: string, metric: boolean) {
+  await logger(name, { komponent: metric }).catch(() => console.warn("Uninitialized amplitude"));
 }
 
-export const logGroupedEvent = (list: string) => {
-  logger("minside-composition", { composition: list }).catch(() => console.warn("Uninitialized amplitude"));
+export const logGroupedEvent = async (list: string) => {
+  await logger("minside-composition", { composition: list }).catch(() => console.warn("Uninitialized amplitude"));
 };
 
-export const logContentEvent = (event: string, value: boolean) => {
-  logger(event, { hasContent: value }).catch(() => console.warn("Uninitialized amplitude"));
+export const logContentEvent = async (event: string, value: boolean) => {
+  await logger(event, { hasContent: value }).catch(() => console.warn("Uninitialized amplitude"));
 };
