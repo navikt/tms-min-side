63c3f663fbe0ce77959b454403b7cc51e7d726d4
sasoria <sergio.alberto.arevalo.soria@nav.no>
2026-05-06 19:56:25 +0200

    Enklere i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
-import { defineConfig, envField } from "astro/config";
-import react from "@astrojs/react";
 import node from "@astrojs/node";
+import react from "@astrojs/react";
+import { defineConfig, envField } from "astro/config";
 
 export default defineConfig({
   base: "/minside",
@@ -24,6 +24,14 @@ export default defineConfig({
       },
     },
   ],
+  i18n: {
+    defaultLocale: "nb",
+    locales: ["nb", "nn", "en"],
+    routing: {
+      prefixDefaultLocale: true,
+      redirectToDefaultLocale: true,
+    },
+  },
   output: "server",
   adapter: node({
     mode: "standalone",
@@ -33,82 +41,82 @@ export default defineConfig({
       AKTUELT_URL: envField.string({
         context: "server",
         access: "secret",
-        default: "http://localhost:3000/aktuelt"
+        default: "http://localhost:3000/aktuelt",
       }),
       UTKAST_URL: envField.string({
         context: "server",
         access: "secret",
-        default: "http://localhost:3000/minside/utkast"
+        default: "http://localhost:3000/minside/utkast",
       }),
       UTKAST_API_URL: envField.string({
         context: "server",
         access: "secret",
-        default: "http://localhost:3000/utkast"
+        default: "http://localhost:3000/utkast",
       }),
       VARSLER_URL: envField.string({
         context: "server",
         access: "secret",
-        default: "http://localhost:3000/minside/varsler"
+        default: "http://localhost:3000/minside/varsler",
       }),
       VARSEL_API_URL: envField.string({
         context: "server",
         access: "secret",
-        default: "http://localhost:3000/varsler"
+        default: "http://localhost:3000/varsler",
       }),
       DIN_OVERSIKT_URL: envField.string({
         context: "server",
         access: "secret",
-        default: "http://localhost:3000/selector/din-oversikt"
+        default: "http://localhost:3000/selector/din-oversikt",
       }),
       MELDEKORT_URL: envField.string({
         context: "server",
         access: "secret",
-        default: "http://localhost:3000/meldekort"
+        default: "http://localhost:3000/meldekort",
       }),
       DOKUMENTARKIV_URL: envField.string({
         context: "server",
         access: "secret",
-        default: "http://localhost:3000/dokumentarkiv"
+        default: "http://localhost:3000/dokumentarkiv",
       }),
       DOKUMENTER_URL: envField.string({
         context: "server",
         access: "secret",
-        default: "http://localhost:3000/journalposter"
+        default: "http://localhost:3000/journalposter",
       }),
       INNBOKS_URL: envField.string({
         context: "server",
         access: "secret",
-        default: "http://localhost:3000/innboks"
+        default: "http://localhost:3000/innboks",
       }),
       NAVN_URL: envField.string({
         context: "server",
         access: "secret",
-        default: "http://localhost:3000/navn"
+        default: "http://localhost:3000/navn",
       }),
       UTBETALINGSOVERSIKT_API_URL: envField.string({
         context: "server",
         access: "secret",
-        default: "http://localhost:3000/utbetalinger/siste"
+        default: "http://localhost:3000/utbetalinger/siste",
       }),
       UTBETALINGSOVERSIKT_URL: envField.string({
         context: "server",
         access: "secret",
-        default: "http://localhost:3000/utbetalingsoversikt"
+        default: "http://localhost:3000/utbetalingsoversikt",
       }),
       REDIRECT_URI: envField.string({
         context: "server",
         access: "secret",
-        default: "http://localhost:4321/minside"
+        default: "http://localhost:4321/minside",
       }),
       PUBLIC_TELEMETRY_URL: envField.string({
         context: "server",
         access: "secret",
-        default: "http://localhost:3000/collect"
+        default: "http://localhost:3000/collect",
       }),
       PUBLIC_LEGACY_SELECTOR_URL: envField.string({
         context: "server",
         access: "secret",
-        default: "http://localhost:3000/selector/din-oversikt"
+        default: "http://localhost:3000/selector/din-oversikt",
       }),
     },
   },
