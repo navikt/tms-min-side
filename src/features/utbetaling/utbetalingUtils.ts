b42ad20ab6b9b778620e0c2f906246d21c0dac15
sasoria <sergio.alberto.arevalo.soria@nav.no>
2026-05-13 15:49:21 +0200

    Bedre conditionals

--- src/features/utbetaling/utbetalingUtils.ts
+++ src/features/utbetaling/utbetalingUtils.ts
@@ -1,6 +1,12 @@
 import dayjs from "dayjs";
 import "dayjs/locale/nb";
+import type { Utbetaling } from "./utbetalingTypes";
 
 export const formatToReadableDate = (date: string) => {
   return dayjs(date).locale("nb").format("D. MMMM ");
 };
+
+export const isKommende = (data: Utbetaling) => data.kommende && !data.sisteUtbetaling;
+export const isIngenUtbetaling = (data: Utbetaling) => !data.kommende && !data.sisteUtbetaling;
+export const isTidligereUtbetaling = (data: Utbetaling) => data.sisteUtbetaling && !data.kommende;
+export const isUtbetalingList = (data: Utbetaling) => data.sisteUtbetaling && data.kommende;
