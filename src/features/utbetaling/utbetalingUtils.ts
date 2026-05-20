420e948ec72c51505092fd443eeb6b19159e1934
sasoria <sergio.alberto.arevalo.soria@nav.no>
2026-05-20 17:07:59 +0200

    Readability

--- src/features/utbetaling/utbetalingUtils.ts
+++ src/features/utbetaling/utbetalingUtils.ts
@@ -5,6 +5,9 @@ import type { Utbetaling } from "./utbetalingTypes";
 export const formatToReadableDate = (date: string) => dayjs(date).locale("nb").format("D. MMMM ");
 
 export const isIngen = (utbetaling: Utbetaling): boolean => !utbetaling.kommende && !utbetaling.sisteUtbetaling;
+
 export const isKommende = (utbetaling: Utbetaling): boolean => !!utbetaling.kommende && !utbetaling.sisteUtbetaling;
+
 export const isTidligere = (utbetaling: Utbetaling): boolean => !!utbetaling.sisteUtbetaling && !utbetaling.kommende;
+
 export const isList = (utbetaling: Utbetaling): boolean => !!utbetaling.sisteUtbetaling && !!utbetaling.kommende;
