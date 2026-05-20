184407e2a5641be015777f1f855fc129f0dd2e48
Sergio A. Arevalo Soria <sergio.alberto.arevalo.soria@nav.no>
2026-05-20 17:24:54 +0200

    Fikser etter typecheck

--- src/features/innloggede-tjenester/link/LinkWithAmplitude.tsx
+++ src/features/innloggede-tjenester/link/LinkWithAmplitude.tsx
@@ -1,21 +1,10 @@
+import type { Lenke } from "@src/features/innloggede-tjenester/innloggedeTjenesterUrls";
 import style from "@src/features/innloggede-tjenester/section/InnloggedeTjensterSection.module.css";
 import { logEvent } from "@src/shared/utils/client/umami.ts";
 import type { Language } from "@src/shared/utils/server/language.ts";
-import React from "react";
-
-interface Link {
-  nb: string;
-  nn: string;
-  en: string;
-  url: {
-    nb: string;
-    nn: string;
-    en: string;
-  };
-}
 
 interface Props {
-  link: Link;
+  link: Lenke;
   language: Language;
 }
 
@@ -23,7 +12,7 @@ const LinkWithAmplitude = ({ link, language }: Props) => (
   <a
     className={style.color}
     href={link.url[language]}
-    onClick={() => logEvent("innloggede-tjenester-lenke", "innloggede-tjenester", link["nb"])}
+    onClick={() => logEvent("innloggede-tjenester-lenke", "innloggede-tjenester", link.nb)}
   >
     {link[language]}
   </a>
