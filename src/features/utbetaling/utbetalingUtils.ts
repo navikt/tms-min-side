ae0f6ad5025912f56a1a04ea19c5d031402db48b
sasoria <sergio.alberto.arevalo.soria@nav.no>
2026-05-20 17:04:20 +0200

    Bruk best practice

--- src/features/utbetaling/utbetalingUtils.ts
+++ src/features/utbetaling/utbetalingUtils.ts
@@ -4,7 +4,7 @@ import type { Utbetaling } from "./utbetalingTypes";
 
 export const formatToReadableDate = (date: string) => dayjs(date).locale("nb").format("D. MMMM ");
 
-export const isKommende = (data: Utbetaling): boolean => !!data.kommende && !data.sisteUtbetaling;
-export const isIngen = (data: Utbetaling): boolean => !data.kommende && !data.sisteUtbetaling;
-export const isTidligere = (data: Utbetaling): boolean => !!data.sisteUtbetaling && !data.kommende;
-export const isList = (data: Utbetaling): boolean => !!data.sisteUtbetaling && !!data.kommende;
+export const isIngen = (utbetaling: Utbetaling): boolean => !utbetaling.kommende && !utbetaling.sisteUtbetaling;
+export const isKommende = (utbetaling: Utbetaling): boolean => !!utbetaling.kommende && !utbetaling.sisteUtbetaling;
+export const isTidligere = (utbetaling: Utbetaling): boolean => !!utbetaling.sisteUtbetaling && !utbetaling.kommende;
+export const isList = (utbetaling: Utbetaling): boolean => !!utbetaling.sisteUtbetaling && !!utbetaling.kommende;
