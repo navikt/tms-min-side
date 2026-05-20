59527fd850d1cd16156b9d08d5f31c3bbdd6bd61
sasoria <sergio.alberto.arevalo.soria@nav.no>
2026-05-20 16:44:49 +0200

    Bedre utbetalingtypes

--- src/features/utbetaling/utbetalingUtils.ts
+++ src/features/utbetaling/utbetalingUtils.ts
@@ -1,10 +1,10 @@
 import dayjs from "dayjs";
 import "dayjs/locale/nb";
-import type { Utbetaling, UtbetalingData } from "./utbetalingTypes";
+import type { Utbetaling } from "./utbetalingTypes";
 
 export const formatToReadableDate = (date: string) => dayjs(date).locale("nb").format("D. MMMM ");
 
-export const isKommendeUtbetaling = (data: Utbetaling) => data.kommende && !data.sisteUtbetaling;
-export const isIngenUtbetaling = (data: Utbetaling) => !data.kommende && !data.sisteUtbetaling;
-export const isTidligereUtbetaling = (data: Utbetaling) => data.sisteUtbetaling && !data.kommende;
-export const isUtbetalingList = (data: Utbetaling) => data.sisteUtbetaling && data.kommende;
+export const isKommende = (data: Utbetaling): boolean => !!data.kommende && !data.sisteUtbetaling;
+export const isIngen = (data: Utbetaling): boolean => !data.kommende && !data.sisteUtbetaling;
+export const isTidligere = (data: Utbetaling): boolean => !!data.sisteUtbetaling && !data.kommende;
+export const isList = (data: Utbetaling): boolean => !!data.sisteUtbetaling && !!data.kommende;
