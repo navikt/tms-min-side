5afb54b5a435c471555aca69a7e949902f97ef30
sasoria <sergio.alberto.arevalo.soria@nav.no>
2026-05-21 17:26:24 +0200

    Refactor middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,12 @@
+import { REDIRECT_URI } from "astro:env/server";
 import { getToken, validateToken } from "@navikt/oasis";
-import { isLocal } from "@src/shared/utils/server/environment.ts";
-import logger from "@src/shared/utils/server/logger";
 import { defineMiddleware } from "astro/middleware";
-import { loginUrl } from "./urls";
-import { isInternal } from "./utils";
+import { isInternal, isLocal } from "./shared/utils/server/environment.ts";
 
 export const onRequest = defineMiddleware(async (context, next) => {
   const token = getToken(context.request.headers);
   const params = encodeURIComponent(context.url.search);
+  const loginUrl = `/minside/oauth2/login?redirect=${REDIRECT_URI}`;
 
   if (isLocal) {
     return next();
