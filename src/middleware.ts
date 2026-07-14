3f0c73fd205a782e83ab439734a13279ee944a44
sasoria <sergio.alberto.arevalo.soria@nav.no>
2026-07-14 18:40:47 +0200

    Formattering

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,5 +4,6 @@ import { sequence } from "astro/middleware";
 export const onRequest = sequence(authenticate(), async (context, next) => {
   const { validation } = context.locals;
   context.locals.isSubstantial = validation.ok && validation.payload.acr === "idporten-loa-substantial";
+
   return next();
 });
